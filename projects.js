aadb506a040e66aa66b0758d63bbbc8157a1d88f
Bartek <bartg1259@gmail.com>
2026-03-31 18:44:09 -0500

    Inital Commit

---
+++ projects.js
@@ -0,0 +1,152 @@
+PROJECTS = [
+  {
+    "name": "Pretraining Chain of Thought Transformer",
+    "category": "Machine Learning & AI",
+    "description": "A project for pretraining a chain of thought transformer model.",
+    "image": "images/pretraining_cot_transformer.png",
+    "link": "https://github.com/bart1259/ChainOfThoughtTransformerTraining",
+    "technologies": ["HF Transformers", "Deep Learning", "Python"],
+    "year": 2025,
+    "featured": "Machine Learning & AI"
+  },
+  {
+    "name": "City Diffusion",
+    "category": "Machine Learning & AI",
+    "description": "A project for generating top down city / suburb layouts using DDPM models.",
+    "image": "images/city_diffusion.gif",
+    "link": "https://github.com/bart1259/CityDiffusion",
+    "technologies": ["HF Diffusers", "Deep Learning", "Python", "Javascript", "HTML"],
+    "year": 2026,
+    "featured": "Machine Learning & AI"
+  },
+  {
+    "name": "Stable Diffusion Clock",
+    "category": "Machine Learning & AI",
+    "description": "A clock that generates a new fantasy image every minute using Stable Diffusion.",
+    "image": "images/diffusion_clock.png",
+    "link": "https://github.com/bart1259/DiffusionClock",
+    "technologies": ["HF Diffusers", "Python", "Javascript", "HTML"],
+    "year": 2025,
+    "featured": "Machine Learning & AI"
+  },
+  {
+    "name": "AI Weather Caster",
+    "category": "Machine Learning & AI",
+    "description": "CNN to predict weather across the US.",
+    "image": "images/weather_caster.png",
+    "link": "https://github.com/bart1259/AIWeatherCaster",
+    "technologies": ["Tensorflow", "Python", "Javascript", "HTML"],
+    "year": 2022,
+    "featured": "Machine Learning & AI"
+  },
+  {
+    "name": "CUDA Accelerated Weaver",
+    "category": "GPU Programming / Computer Graphics",
+    "description": "Using Cuda to accelerate a greedy weaving algorithm for colorful weave art.",
+    "image": "images/birds.gif",
+    "link": "https://github.com/bart1259/CudaWeaver",
+    "technologies": ["CUDA", "C++"],
+    "year": 2022,
+    "featured": "GPU Programming / Computer Graphics"
+  },
+  {
+    "name": "Py Portal Raytracer",
+    "category": "GPU Programming / Computer Graphics",
+    "description": "Using OpenCL for \"Non-euclidean\" ray tracing. Accelerated with BVH traversal.",
+    "image": "images/portal_ray_tracer.gif",
+    "link": "https://github.com/bart1259/py-portal-raytracer",
+    "technologies": ["OpenCL", "Python"],
+    "year": 2025,
+    "featured": "GPU Programming / Computer Graphics"
+  },
+  {
+    "name": "C# OpenCL Realtime Ray Tracer",
+    "category": "GPU Programming / Computer Graphics",
+    "description": "Using OpenCL to accelerate a ray tracing algorithm for real-time rendering.",
+    "image": "images/ray_tracer.png",
+    "link": "https://github.com/bart1259/CLRayTracer",
+    "technologies": ["C#", "OpenCL"],
+    "year": 2020,
+    "featured": "GPU Programming / Computer Graphics"
+  },
+  {
+    "name": "JAX SPH Simulation",
+    "category": "GPU Programming / Computer Graphics",
+    "description": "Using JAX, a smoothed particle hydrodynamics simulation for fluid dynamics.",
+    "image": "images/sph.gif",
+    "link": "https://github.com/bart1259/Simulations?tab=readme-ov-file#sph-3d-fluid-simulation-rendered-in-blender",
+    "technologies": ["JAX", "Python", "Blender"],
+    "year": 2025,
+    "featured": "GPU Programming / Computer Graphics"
+  },
+  {
+    "name": "Global Illumination",
+    "category": "GPU Programming / Computer Graphics",
+    "description": "A simple global illumination renderer using WebGL and GLSL in 2D and 3D.",
+    "image": "images/global_illumination.png",
+    "link": "https://github.com/bart1259/GlobalIllumination",
+    "technologies": ["WebGL", "GLSL", "Javascript", "HTML"],
+    "year": 2026,
+    "featured": "GPU Programming / Computer Graphics"
+  },
+  {
+    "name": "Planetary Delivery",
+    "category": "Unity Games + Projects",
+    "description": "Drive around a planet picking up and delivering packages within a time limit.",
+    "image": "images/planetary_delivery.png",
+    "link": "https://github.com/bart1259/TetraDefense",
+    "technologies": ["Unity", "C#"],
+    "year": 2026,
+    "featured": "Unity / Games"
+  },
+  {
+    "name": "Tetra Defense",
+    "category": "Unity Games + Projects",
+    "description": "Tower defense game where platforms and towers are shaped like tetris pieces.",
+    "image": "images/tetra_defense.png",
+    "link": null,
+    "technologies": ["Unity", "C#"],
+    "year": 2026,
+    "featured": "Unity / Games"
+  },
+  {
+    "name": "Hex Planet Generator",
+    "category": "Unity Games + Projects",
+    "description": "Unity framework for creating planets made from hexagons",
+    "image": "images/hex_planet.png",
+    "link": null,
+    "technologies": ["Unity", "C#"],
+    "year": 2022,
+    "featured": "Unity / Games"
+  },
+  {
+    "name": "Bart DB",
+    "category": "Other Projects",
+    "description": "A distributed on-disk database implementation featuring a server and client.",
+    "image": "https://cdn.pixabay.com/photo/2020/03/17/17/36/database-4941301_1280.png",
+    "link": "https://github.com/bart1259/BartDB",
+    "technologies": ["C++", "Bash"],
+    "year": 2024,
+    "featured": "Other"
+  },
+  {
+    "name": "Mamba CPU",
+    "category": "Other Projects",
+    "description": "A custom Verilog 32-bit CPU architecture featuring an L1 cache, multi-stage pipeline, custom assembler, and simulator. Synthesis through OpenLane.",
+    "image": "images/mamba_cpu.png",
+    "link": "https://github.com/bart1259/MambaCPU",
+    "technologies": ["Verilog", "OpenLane", "C++", "Python"],
+    "year": 2025,
+    "featured": "Other"
+  },
+  {
+    "name": "Manhunt Game",
+    "category": "Other Projects",
+    "description": "A multiplayer game played in real life, where players hunt down a target with only a 5 minute delayed GPS location. Deployed in AWS using Teraform.",
+    "image": "images/manhunt_game.png",
+    "link": "https://github.com/bart1259/ManHuntGame",
+    "technologies": ["AWS", "Terraform", "Javascript", "HTML"],
+    "year": 2023,
+    "featured": "Other"
+  }
+]
